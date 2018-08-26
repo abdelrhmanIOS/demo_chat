646596b6b7684e0aa976319a2d3d136b0183066e
obada <abdelrhman.java.developer@gmail.com>
2018-08-26 22:38:16 +0200

    first commit

---
+++ gamechatt/trtViewController.swift
@@ -0,0 +1,35 @@
+//
+//  trtViewController.swift
+//  gamechatt
+//
+//  Created by Abdo on 8/16/18.
+//  Copyright © 2018 Abdo. All rights reserved.
+//
+
+import UIKit
+
+class trtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
